28db1101ffdf6838579ac77546f5117270cb6b3a
samwelnella <danielsamwel@gmail.com>
2020-05-19 13:30:18 -0400

    Update kbb_to_lunchmoney.py

--- kbb_to_lunchmoney.py
+++ kbb_to_lunchmoney.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 mileage = '24000'
 
-#Get this URL by going to kbb.com and following the flow for finding your cars value. Copy the page URL once KBB gives you the price
+#Get this URL by going to kbb.com and following the flow for finding your cars value. Copy the page URL once KBB gives you the price. Replace the mileage in the URL with {} to insert your mileage from the script variable.
 car1_url = 'https://www.kbb.com/bmw/5-series/2018/540i-xdrive-sedan-4d/?vehicleid=431625&intent=trade-in-sell&mileage={}&pricetype=private-party&condition=verygood&options=8121036|true'.format(mileage)
 
 browser = webdriver.Firefox()
